4fe3a1ece2971bae97eebb7e84f13f78f102dcd2
Kevin Garbe <kevin@garbe.com>
2021-12-24 10:47:39 -0800

    Remove unfinished heavy hitters code. FRQ and hash left in.

--- net_share.h
+++ net_share.h
@@ -92,9 +92,6 @@ int recv_fmpz_batch(const int sockfd, fmpz_t* const x, const size_t n);
 int send_seed(const int sockfd, const flint_rand_t x);
 int recv_seed(const int sockfd, flint_rand_t x);
 
-int send_heavycfg(const int sockfd, const HeavyConfig x);
-int recv_heavycfg(const int sockfd, HeavyConfig &x);
-
 /* Share functions */
 
 // Unused
--- types.h
+++ types.h
@@ -52,8 +52,6 @@ enum messageType {
     STDDEV_OP,
     LINREG_OP,
     FREQ_OP,
-    COUNTMIN_OP,
-    HEAVY_OP,
 };
 
 struct initMsg {
@@ -61,31 +59,6 @@ struct initMsg {
     unsigned int num_bits;
     unsigned int num_of_inputs;
     unsigned int max_inp;
-};
-
-struct HeavyConfig {
-    /* Target parameters
-    Return all heavy > t
-    With odds (1 - delta), nothing < (1 - eps) t is returned
-    */
-    double t;  // Target heavy. 0.1 is >10%
-    // double eps;
-    // double delta;
-
-    /* Tree params
-    For full tree eps/delta, per layer eps/delta values
-    Since recersive, can be slightly more efficient
-    */
-    // double eps2;  // 1-eps = (1 - eps)^L, so eps' = 1 - log_L (1 - eps)
-    // double delta2;  // delta' = delta (1 - eps) t / 2 L
-    size_t L;  // # layers, input_bits - ceil(log_2(w d))
-
-    /* Input parameters
-    w = a / eps t, d = log_a(1 / delta)
-    For tree, use eps2, delta2 in place
-    */
-    size_t w;  // hash range
-    size_t d;  // # hashes
 };
 
 #endif
