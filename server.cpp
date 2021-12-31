497c0b3ce35177af82966fa27ebbd69aba1c6f44
Kevin Garbe <kevin@garbe.com>
2021-12-31 10:31:44 -0800

    misc cleanup

--- server.cpp
+++ server.cpp
@@ -1738,8 +1738,8 @@ int main(int argc, char** argv) {
                 pair.second -> setCheckerPrecomp(randomX);
         }
 
-        // correlated_store->maybeUpdate();
-        correlated_store->printSizes();
+        correlated_store->maybeUpdate();
+        // correlated_store->printSizes();
 
         socklen_t addrlen = sizeof(addr);
 
